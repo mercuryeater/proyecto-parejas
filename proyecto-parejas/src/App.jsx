0dee299afa8781bb2ef1d0132822353c6b04e391
Federico <federicoc03@gmail.com>
2023-04-28 18:18:54 -0500

    al dia

--- proyecto-parejas/src/App.jsx
+++ proyecto-parejas/src/App.jsx
@@ -2,26 +2,10 @@ import { useState } from "react";
 import "./App.css";
 import AddMovie from "./Components/Add/MovieForm";
 import MovieList from "./Components/Add/movieList";
-
-const pelis = [
-  {
-    id: 1,
-    name: "lord of the rings: the two towers",
-    rating: "8.8/10",
-    genre: "Action",
-    year: "2002",
-  },
-  {
-    id: 2,
-    name: "lord of the rings: the fellowship of the rings",
-    rating: "8.1/10",
-    genre: "Action",
-    year: "1998",
-  },
-];
+import {movies as movieList} from './assets/data'
 
 function App() {
-  const [movies, setMovies] = useState(pelis);
+  const [movies, setMovies] = useState(movieList);
   const [selectMovie, setSelectMovie] = useState(null)
 
   const handleAddMovie = (newMovie) => {
--- proyecto-parejas/src/assets/data.js
+++ proyecto-parejas/src/assets/data.js
@@ -13,6 +13,20 @@ export const movies = [
       genre: 'Science Fiction',
       rating: '7'
     },
+    {
+      id: 1,
+      name: "lord of the rings: the two towers",
+      rating: "8.8/10",
+      genre: "Action",
+      year: "2002",
+    },
+    {
+      id: 2,
+      name: "lord of the rings: the fellowship of the rings",
+      rating: "8.1/10",
+      genre: "Action",
+      year: "1998",
+    },
     // Agregar más contactos aquí...
   ];
   
