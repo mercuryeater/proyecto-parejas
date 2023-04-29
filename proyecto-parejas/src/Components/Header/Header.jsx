7a84c15b5f7cde00255eb69fe6488321e82caab1
Federico <federicoc03@gmail.com>
2023-04-29 11:40:06 -0500

    Header half done

--- proyecto-parejas/src/Components/Header/Header.jsx
+++ proyecto-parejas/src/Components/Header/Header.jsx
@@ -1,15 +1,22 @@
 import React from "react";
+import "./Header.css";
 
 function Header() {
   return (
     <div className="header">
       <div className="header__left">
-        <img src="/path/to/logo.png" alt="Generic Logo" />
-        <h1>Personal Movie Rating</h1>
+        <div className="header__left__container">
+          <img
+            src="./logo.png"
+            alt="Generic Logo"
+            className="header__left__container__logo"
+          />
+        </div>
+        <h2>Personal Movie Rating</h2>
       </div>
-      <div className="header__left">
-      <h3>something</h3>
-      </div>      
+      <div className="header__right">
+        <h3>About Us</h3>
+      </div>
     </div>
   );
 }
